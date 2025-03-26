c6a0e1eb7c222b58f87c96289ced0cd957860f42
Eitor Correa <eitor.candido.correa@gmail.com>
2025-03-26 16:10:58 -0300

    Erro ao nao preencher

---
+++ script.js
@@ -0,0 +1,19 @@
+function fazerlogin() {
+    const usuario = document.getElementById("input-usuario").value.trim
+    const senha = document.getElementById("input-senha").value.trim()
+
+    if (!usuario || !senha)
+        mostrarAlerta("Por favor, preencha todos os campos obrigatórios.")
+        return;
+     
+}
+
+function mostrarAlerta(mensagem){
+    document.getElementById("mensagemAlerta").textContent = mensagem ;
+    document.getElementById("Alerta").style.display = "block" ;
+}
+
+function fecharAlerta() {
+    document.getElementById("Alerta").style.display = "none";
+}
+
